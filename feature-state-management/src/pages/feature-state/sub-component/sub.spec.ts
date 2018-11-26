b59ac7edc90284c6db78e567109ff33e95382a24
Ammar Haider <ammar.haider@bjss.com>
2018-11-26 17:28:54 +0000

    cleans up unit test

--- feature-state-management/src/pages/feature-state/sub-component/sub.spec.ts
+++ feature-state-management/src/pages/feature-state/sub-component/sub.spec.ts
@@ -1,31 +1,17 @@
-import { rootStateReducer } from './../../../app/app.state.reducer';
-import { featureReducer } from '../feature-state.reducer';
 import { SubComponent } from './sub';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-import { Store, StoreModule } from '@ngrx/store';
-
 describe('Home', () => {
   let fixture: ComponentFixture<SubComponent>;
   let component: SubComponent;
-  let store: Store<number>;
 
   beforeEach(() => {
 
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
-      declarations: [SubComponent],
-      imports: [
-        StoreModule.forRoot({
-          rootState: rootStateReducer,
-          count: featureReducer
-        })
-      ]
+      declarations: [SubComponent]
     }).compileComponents();
-
-    store = TestBed.get(Store);
-    jest.spyOn(store, 'dispatch');
 
     fixture = TestBed.createComponent(SubComponent);
     component = fixture.componentInstance;
