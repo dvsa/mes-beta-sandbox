80fe72908fc6e4549aee9fb71a1a163c6d382534
Ammar Haider <ammar.haider@bjss.com>
2018-11-26 15:42:35 +0000

    refactors sub component

--- feature-state-management/src/pages/feature-state/sub-component/sub.ts
+++ feature-state-management/src/pages/feature-state/sub-component/sub.ts
@@ -1,35 +1,21 @@
-import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Increment, Decrement } from '../feature-state.actions';
-import { LogIn, LogOut } from '../../../app/app.state.actions';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'sub',
   templateUrl: 'sub.html'
 })
 export class SubComponent {
+  
+  // Dispatch using inputs
+  @Input() increment;
+  @Input() decrement;
+  @Input() logIn;
+  @Input() logOut;
 
   text: string;
 
-  constructor(private store: Store<{ count: number }>) {
-    console.log('Hello SubComponent Component');
+  constructor() {
     this.text = 'SubComponent';
-  }
-
-  increment() {
-    this.store.dispatch(new Increment());
-  }
- 
-  decrement() {
-    this.store.dispatch(new Decrement());
-  }
-
-  logIn() {
-    this.store.dispatch(new LogIn())
-  }
-
-  logOut() {
-    this.store.dispatch(new LogOut())
   }
 
 }
