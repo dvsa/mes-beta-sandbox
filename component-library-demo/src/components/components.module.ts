67f4dd0e1ff16da9e42ec07bbc3217b22d82cc4b
Tom Billington <t.billington@kainos.com>
2018-11-28 13:17:07 +0000

    Added Component Lib Demo

---
+++ component-library-demo/src/components/components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MesButtonComponent } from './mes-button/mes-button';
+import { IonicModule } from 'ionic-angular';
+@NgModule({
+	declarations: [MesButtonComponent],
+	imports: [IonicModule],
+	exports: [MesButtonComponent]
+})
+export class ComponentsModule {}
