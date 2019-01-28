7b91fd927d6b08dbff622819a7f0df5df76670ab
Tom Billington <t.billington@kainos.com>
2019-01-28 10:10:15 +0000

    Added UXCam Demo

--- usability/src/pages/uxcam/uxcam.ts
+++ usability/src/pages/uxcam/uxcam.ts
@@ -9,14 +9,18 @@ import { IonicPage, NavController } from 'ionic-angular';
 export class UxcamPage {
 
    
-  uxCam = (<any>window).UxCam;
+  uxCam = (<any>window).UXCam;
   apiKey = '3r69p8gxk3v80m8'
 
   constructor(public navCtrl: NavController) {
 
   }
   startRecording() {
-    this.uxCam.startWithKey(this.apiKey)
+    this.uxCam.startWithKey(this.apiKey);
   };
 
+
+  buttonPress(buttonNumber: number) {
+    this.uxCam.logEvent('I clicked button' + buttonNumber);
+  }
 }
